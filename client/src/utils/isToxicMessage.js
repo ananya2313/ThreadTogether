a1658a934a4f1ff3c985d36c1276242cf28d0869
Ananya <ananyaoct24@gmail.com>
2025-08-25 20:29:39 +0530

    Changes on client side

--- client/src/utils/isToxicMessage.js
+++ client/src/utils/isToxicMessage.js
@@ -1,6 +1,8 @@
+const SERVER_URL = process.env.VITE_BACKEND_URL;
+require('dotenv').config();
 export const isToxicMessage = async (text) => {
   try {
-    const res = await fetch("http://localhost:5000/api/moderation", {
+    const res = await fetch(`${SERVER_URL}/api/moderation`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text }), // ✅ correct key
